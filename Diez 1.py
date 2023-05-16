97dc585b2ffac5486112b45a3db373669fb5bd2f
Alejandra Arenas <124609988+alejayz@users.noreply.github.com>
2023-05-15 21:31:58 -0500

    Add files via upload

---
+++ Diez 1.py
@@ -0,0 +1,16 @@
+#Calcular el promedio de un arreglo de reales
+
+def Reales(a:float, b:float, c:float, d:float)-> float:  #se indican los elementos de la lista
+    lista=[a, b, c, d]
+    suma = 0  #inicia la suma en cero
+    for i in lista:
+        suma += i #suma todos los elementos de la lista
+    return suma / len(lista) # divide la suma entre el total de elememtos para hallar el promedio
+
+if __name__ == "__main__":
+    a = float(input("ingresar un número real: "))
+    b = float(input("ingresar un número real: "))
+    c = float(input("ingresar un número real: "))
+    d = float(input("ingresar un número real: "))
+    PromArreglo = Reales(a,b,c,d) #se evalúa la función en los valores ingresados
+    print("el promedio de " + str(a) + ', ' + str(b) + ', ' + str(c) +', ' + str(d) + " es: " + str(PromArreglo))
